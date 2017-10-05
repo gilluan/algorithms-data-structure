0795e0fc392e9968590e06ae6c480f24ce514109
Gilluan Formiga <gilluan@hotmail.com.br>
2017-10-05 15:49:41 -0300

    correção

--- javascript/listRevert.js
+++ javascript/listRevert.js
@@ -2,7 +2,7 @@
 
     let reverter = ([head, ...tail], result = []) => {
         if(head != 0 && !head) return result;
-        return reverter(tail, [...[head], ...result]);
+        return reverter(tail, [head, ...result]);
     }
     console.log(reverter([0,1,2,3,4,5,6])); //6,5,4,3,2,1
 })()
